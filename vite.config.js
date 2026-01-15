488ad16d8c2c1fe861a275ad6162a4f862b4a93e
velocibet <honoka4h@gmail.com>
2026-01-16 01:49:23 +0900

    vite config modification

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        post: resolve(__dirname, 'post.html'),
+      },
+    },
+  },
 })
