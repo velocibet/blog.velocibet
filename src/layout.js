ca4b5b154e9c10857edc965b8a223e3f9842528a
velocibet <honoka4h@gmail.com>
2026-01-16 01:16:39 +0900

    fix UI

--- src/layout.js
+++ src/layout.js
@@ -1,4 +1,14 @@
-document.querySelectorAll("[data-include]").forEach(async el => {
-  const res = await fetch(el.dataset.include);
-  el.innerHTML = await res.text();
+import header from "./layouts/header.html?raw";
+import footer from "./layouts/footer.html?raw";
+
+const includes = {
+  "/layouts/header.html": header,
+  "/layouts/footer.html": footer,
+};
+
+document.querySelectorAll("[data-include]").forEach(el => {
+  const key = el.dataset.include;
+  if (includes[key]) {
+    el.innerHTML = includes[key];
+  }
 });
