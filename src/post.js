bd898f523a393923983b77bf594066f6b69490ab
velocibet <honoka4h@gmail.com>
2026-01-16 12:39:25 +0900

    add favicon and set meta data

--- src/post.js
+++ src/post.js
@@ -11,6 +11,30 @@ const date = document.querySelector("#date");
 const contentElement = document.querySelector(".content");
 const tagsElement = document.querySelector(".tags");
 
+function setMeta(name, content, isProperty = false) {
+  const selector = isProperty
+    ? `meta[property="${name}"]`
+    : `meta[name="${name}"]`;
+
+  let tag = document.querySelector(selector);
+
+  if (!tag) {
+    tag = document.createElement("meta");
+    isProperty ? tag.setAttribute("property", name) : tag.setAttribute("name", name);
+    document.head.appendChild(tag);
+  }
+
+  tag.setAttribute("content", content);
+}
+
+function extractFirstImage(html) {
+  const temp = document.createElement("div");
+  temp.innerHTML = html;
+
+  const img = temp.querySelector("img");
+  return img ? img.getAttribute("src") : null;
+}
+
 function getCategoryName(categoryId, categories) {
   const foundCategory = categories.find(cate => cate.id === categoryId);
   return foundCategory ? foundCategory.name : categoryId;
@@ -41,6 +65,21 @@ async function setDocument() {
 
     const tagHtml = attributes.tags.map(t => `#${t}`).join(" ");
     tagsElement.innerHTML = `<p>태그</p> <span>${tagHtml}</span>`;
+
+    document.title = `${attributes.title}`;
+    setMeta("description", attributes.summary);
+    setMeta("og:title", attributes.title, true);
+
+    const firstImage = extractFirstImage(content);
+    setMeta(
+      "og:image",
+      firstImage
+        ? firstImage.startsWith("http")
+          ? firstImage
+          : `https://blog.velocibet.com${firstImage}`
+        : "https://blog.velocibet.com/images/header.jpg",
+      true
+    );
   } catch(err) {
     console.error("게시글 및 카테고리를 불러오던 도중 에러가 발생했습니다.", err)
   }
