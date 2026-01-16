5d199999b56f24b38cac25fb1e12133170612951
velocibet <honoka4h@gmail.com>
2026-01-16 13:08:35 +0900

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
+        policy: resolve(__dirname, 'policy.html'),
         post: resolve(__dirname, 'post.html'),
       },
     },
